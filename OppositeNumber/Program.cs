42acaf72df0fa780556c93bb7377fccc4ad051f2
Adorable <Adorable@DESKTOP-TCRBLA8>
2023-04-12 18:15:27 +0300

    Добавьте файлы проекта.

---
+++ OppositeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace OppositeNumber
+{
+    /*
+     * Very simple, given an integer or a floating-point number, find its opposite. 
+     * 
+     * Examples:
+     * 
+     * 1: -1
+     * 14: -14
+     * -34: 34
+     * 
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, чтобы получить его противоположность:");
+
+            double Number = double.Parse(Console.ReadLine());
+
+            Number = -Number;
+
+            Console.WriteLine(Number);
+        }
+    }
+}
---
+++ OppositeNumber/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("OppositeNumber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OppositeNumber")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("967841cc-9100-4e03-bf87-42222b65e048")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
